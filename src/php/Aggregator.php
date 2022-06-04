33d197109dcece6461ca5322e591c6509d666fb9
Tony Gravagno <xh8r2nbu9q@snkmail.com>
2022-06-03 17:34:09 -0700

    Revisions to freewebheaders files and processing

--- src/php/Aggregator.php
+++ src/php/Aggregator.php
@@ -13,14 +13,15 @@ class Aggregator
 
         $this->write(self::RELATIVE . 'turalus/turalus_en.txt');
         $this->write(self::RELATIVE . 'banned_whole_usernames/biglou_resources_bad-words.txt');
+        $this->write(self::RELATIVE . 'banned_whole_usernames/freewebheaders.txt');
 
 
 
-        $final = file(self::RELATIVE . self::WRITEPATH, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-        sort($final);
-        $unique = array_unique($final);
-
-        $result = file_put_contents(self::RELATIVE . self::WRITEPATH, implode("\r\n",  $unique));
+        $dataArray = file(self::RELATIVE . self::WRITEPATH, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        sort($dataArray);
+        $dataArray = array_unique($dataArray);
+        $dataArray = $this->removeEOL($dataArray);
+        $result = file_put_contents(self::RELATIVE . self::WRITEPATH, implode(PHP_EOL,  $dataArray));
         if (false === $result)
             die("error writing to self::WRITEPATH");
     }
@@ -28,10 +29,18 @@ class Aggregator
     private function write($path)
     {
         $to_add = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        $to_add = $this->removeEOL($to_add);
         $prefix = file_exists(self::RELATIVE . self::WRITEPATH) ? PHP_EOL : '';
-        $result = file_put_contents(self::RELATIVE . self::WRITEPATH, $prefix . implode("\r\n",  $to_add),  FILE_APPEND);
+        $result = file_put_contents(self::RELATIVE . self::WRITEPATH, $prefix . implode(PHP_EOL,  $to_add),  FILE_APPEND);
         if (false === $result)
             die("error writing $path to self::WRITEPATH");
+    }
+    private function removeEOL($dataArray)
+    {
+        foreach ($dataArray as &$value) {
+            $value = str_replace(["\r", "\n"], "", $value);
+        }
+        return $dataArray;
     }
 }
 (new Aggregator())->aggregate();
